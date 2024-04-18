d81d02c0bb58213cccc1f4ad77e99b2e7d573226
shreexe <shreeni.gan@gmail.com>
2024-04-18 13:07:21 +0530

    result.html

--- app.py
+++ app.py
@@ -5,12 +5,15 @@ from pypdf import PdfReader
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+from octoai.client import Client
+
 
 app = Flask(__name__)
 
 
+client = Client()
 # Initialize Groq client
-client = Groq(api_key=os.environ.get('GROQ_API_KEY'))
+# client = Groq(api_key=os.environ.get('GROQ_API_KEY'))
 
 # Function to extract text from a PDF file
 
@@ -37,25 +40,26 @@ def get_text_from_url(url):
 
 
 def get_faqs(content):
-    # print(content,"content")
+    
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": 'system',
                 'content': """Your role is to help users extract content from documents contents like terms of services they provide. Once you have the content, you will thoroughly read through it and generate questions that could be considered for a FAQ section for each content segment. Additionally, you will create concise answers for these questions, referencing the specific section or topic from which the answer was derived. carefully analyzing documents to identify key points that could form the basis of frequently asked questions.
-Ensure accuracy in content extraction and interpretation. Give the content of each question in a new line.  Mention where you get the answer from at last of the respective answer."""
+Ensure accuracy in content extraction and interpretation. Give the content of each question in a new line and in a proper html format where heading is in h2 tag, Q and A are in seperate paragraph tags.  Mention where you get the answer from at last of the respective answer."""
             },
             {
                 'role': 'user',
                 'content': "Here is the content I would like to generate FAQs for:" + content+"\nGenerate FAQs for each topic."
             }
         ],
-        model="mixtral-8x7b-32768",
-        temperature=0.8,
+        model="mixtral-8x7b-instruct",
+        temperature=0.3,
         stream=True,
     )
     print(chat_completion, "chat_completion")
     return chat_completion
+
 
 # Flask route for the home page
 
@@ -67,15 +71,15 @@ def home():
 # Flask route to handle form submission
 
 
-@app.route('/process', methods=['POST', 'GET'])
+@app.route('/process', methods=['POST'])
 def process():
     url = request.form.get('url')
     file = request.files['file']
 
     if url:
         text = get_text_from_url(url)
-        print(text,"%textt!!")
-       
+        print(text, "%textt!!")
+
     elif file:
         text = get_text_from_document(file)
     else:
@@ -85,7 +89,7 @@ def process():
     for chunk in get_faqs(text):
         if chunk.choices[0].delta.content:
             faqs += chunk.choices[0].delta.content
-           
+            print(faqs,"FAQ")
 
     return render_template('result.html', faqs=faqs)
 
