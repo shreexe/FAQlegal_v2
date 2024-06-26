4a29b9407b155fd4218c62010865dd2b60771b1d
shreexe <shreeni.gan@gmail.com>
2024-06-26 23:24:03 +0530

    v2 prompt

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from octoai.client import Client
 from openai import OpenAI
+import time
 
 
 load_dotenv()
@@ -45,12 +46,8 @@ def get_faqs(content):
         messages=[
             {
                 "role": 'system',
-                'content': """Your role is to help users extract content from documents contents like terms of services they provide. 
-                Once you have the content, you will thoroughly read through it and generate questions that could be considered for a FAQ 
-                section for each content segment. Additionally, you will create concise answers for these questions, referencing the 
-                specific section or topic from which the answer was derived. Give the response in html tags. Make sure you give headings for the FAQs generated within h5. 
-                give response where the heading of each section is in h5 tag. let the question and answer be in p tag, normal font, and break after each answer. The heading in h5 tag, bold tag. The question and answer in seperate p tag without strong tag.
-                Make sure to complete the response with respective html tags"""
+                'content': """Your role is to help the users in generating FAQs in the legal context from the documents provided.Once you get the content, construct three consise and accurate question-answer pairs for each section. Provide the section from where you referred.
+                               Thoroughly read the entire text given and complete the response by providing FAQs as said. Be accurate and mention the sections."""
             },
             {
                 'role': 'user',
@@ -59,36 +56,86 @@ def get_faqs(content):
         ],
         model="mixtral-8x7b-instruct",
         temperature=0.1,
-        stream=True,
+        stream=False,
         max_tokens=4096
 
 
     )
+    print(chat_completion, "----FAQ")
 
-    return chat_completion
+    # faqs = ''
+    # for chunk in get_faqs(chat_completion):
+    #     if chunk.choices[0].delta.content:
+    #         faqs += chunk.choices[0].delta.content
+
+    return get_format(chat_completion.choices[0].message.content)
+
+
+def get_format(content):
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": 'system',
+                'content': """You have to provide the html tags for the given content. h5 and bold tag for the heading. label in bold where questions as 'Q' and answers as 'A' in bold. Don't provide questions or answers in bold.
+                           Provide a break after every answer.Thoroughly follow the instruction through out the content provided. Don't mix the tags and stick to the instructions strictly."""
+            },
+            {
+                'role': 'user',
+                'content': "Here is the content I would like to the HTML tags for " + content+"\nProvide corressponding HTML tags."
+            }
+        ],
+        model="mixtral-8x7b-instruct",
+        temperature=0.1,
+        stream=False,
+        max_tokens=4096
+
+
+    )
+    print(chat_completion, "-----FORMAT")
+
+    return chat_completion.choices[0].message.content
 
 
 def openai(content):
-    stream = client1.chat.completions.create(
+    chat = client1.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{
             "role": 'system',
-            'content': """Your role is to help users extract content from documents contents like terms of services they provide. 
-                Once you have the content, you will thoroughly read through it and generate questions that could be considered for a FAQ 
-                section for each content segment. Additionally, you will create concise answers for these questions, referencing the 
-                specific section or topic from which the answer was derived. Give the response in html tags. Make sure you give headings for the FAQs generated within h5. 
-                give response where the heading of each section is in h5 tag. let the question and answer be in p tag, normal font, and break after each answer. The heading in h5 tag, bold tag. The question and answer in seperate p tag without strong tag.
-                Make sure to complete the response with respective html tags"""
+            'content': """Your role is to help the users in generating FAQs in the legal context from the documents provided.Once you get the content, construct three consise and accurate question-answer pairs for each section. Provide the section from where you referred.
+                               Thoroughly provide the FAQs for the entire text given and complete the response. Be accurate and mention the sections."""
         },
             {
             'role': 'user',
                 'content': "Here is the content I would like to generate FAQs for:" + content+"\nGenerate FAQs for each topic."
         }
         ],
-        stream=True,
+        stream=False,
         temperature=0.1
     )
-    return stream
+    print(chat, "----CHATGPT")
+    return openai_format(chat.choices[0].message.content)
+
+
+def openai_format(content):
+    chat = client1.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{
+            "role": 'system',
+            'content': """You have to provide the html tags for the given content. h5 and bold tag for the heading. bold label questions and answers as  'Q' and 'A' respectively. Don't provide questions or answers in bold.
+                           Provide a break after every answer.Thoroughly follow the instruction through out the content provided. Don't mix the tags and stick to the instructions strictly. Heading in h5 and bold.
+                         Make sure to complete the response with respective html tags as plain text without triple backticks"""
+        },
+            {
+            'role': 'user',
+                'content': "Here is the content I would like to generate FAQs for:" + content+"\nGenerate FAQs for each topic."
+        }
+        ],
+        stream=False,
+        temperature=0.1
+    )
+    print(chat, "----FORMAT")
+    return chat.choices[0].message.content
 
 
 @app.route('/')
@@ -100,6 +147,8 @@ def home():
 
 @app.route('/process', methods=['POST'])
 def process():
+    start = time.time()
+
     url = request.form.get('url')
     file = request.files['file']
 
@@ -111,17 +160,16 @@ def process():
     else:
         return "No input provided"
 
-    faqs = ''
-    for chunk in get_faqs(text):
-        if chunk.choices[0].delta.content:
-            faqs += chunk.choices[0].delta.content
-            print(faqs, "---FROM OCTO AI")
+    faqs = get_faqs(text)
+    end = time.time()
+    print("****Time*******", (end-start))
 
     return render_template('result.html', faqs=faqs)
 
 
 @app.route('/chatgpt', methods=['POST'])
 def chatgpt():
+    start = time.time()
     url = request.form.get('url')
     file = request.files['file']
 
@@ -133,11 +181,10 @@ def chatgpt():
     else:
         return "No input provided"
 
-    faqs = ''
-    for chunk in get_faqs(text):
-        if chunk.choices[0].delta.content:
-            faqs += chunk.choices[0].delta.content
-            print(faqs, "---FROM GPT")
+    faqs = openai(text)
+
+    end = time.time()
+    print("****Time*******", (end-start))
 
     return render_template('result.html', faqs=faqs)
 
